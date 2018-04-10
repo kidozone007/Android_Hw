39a95fdec99ad9d2c260b459fdf4b3d30409a9b9
105590014 <kidozone007@gmail.com>
2018-04-10 21:18:13 +0800

    Hw4_1

--- app/src/main/java/edu/ntut/user/myhw3/MainActivity.java
+++ app/src/main/java/edu/ntut/user/myhw3/MainActivity.java
@@ -1,105 +1,156 @@
 package edu.ntut.user.myhw3;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner mSpnSex;
-    private RadioGroup mRadGrp;
-    private RadioButton mRadBtnAgeRange1;
-    private RadioButton mRadBtnAgeRange2;
-    private RadioButton mRadBtnAgeRange3;
-    private TextView mTxtNumFamily;
-    private NumberPicker mNumPkrFamily;
-    private Button mBtnOK;
-    private TextView mTxtSug;
+    private MainActivity self;
 
-    private String selectedSex;
+    private Spinner mSpAge;
+    private RadioGroup mRadGrpSex;
+    private RadioButton mRadBtnSexRange1;
+    private RadioButton mRadBtnSexRange2;
+    private CheckBox mMusic,mSing,mDance,mTravel,mReading,mWriting,mClimbing,mSwim,mEating,mDrawing;
+    private TextView mTxtSug;
+    private TextView mTxtHub;
+
+    private Button mBtnOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        self = this;
 
-        mSpnSex = (Spinner) findViewById(R.id.spnSex);
-        mRadGrp = (RadioGroup) findViewById(R.id.radGrpAge);
-        mRadBtnAgeRange1 = (RadioButton) findViewById(R.id.radBtnAgeRange1);
-        mRadBtnAgeRange2 = (RadioButton) findViewById(R.id.radBtnAgeRange2);
-        mRadBtnAgeRange3 = (RadioButton) findViewById(R.id.radBtnAgeRange3);
-        mTxtNumFamily = (TextView) findViewById(R.id.txtNumFamily);
-        mNumPkrFamily = (NumberPicker) findViewById(R.id.numPkrFamply);
-        mNumPkrFamily.setMinValue(0);
-        mNumPkrFamily.setMaxValue(20);
-        mNumPkrFamily.setValue(3);
+        mRadGrpSex = (RadioGroup) findViewById(R.id.radGrpSex);
+        mRadBtnSexRange1 = (RadioButton) findViewById(R.id.radBtnSexRange1);
+        mRadBtnSexRange2 = (RadioButton) findViewById(R.id.radBtnSexRange2);
+        mSpAge = (Spinner) findViewById(R.id.spnAge);
+
+        mMusic=(CheckBox) findViewById(R.id.chkHabit_Music);
+        mSing=(CheckBox) findViewById(R.id.chkHabit_Sing);
+        mDance=(CheckBox) findViewById(R.id.chkHabit_Dance);
+        mTravel=(CheckBox) findViewById(R.id.chkHabit_Travel);
+        mReading=(CheckBox) findViewById(R.id.chkHabit_Reading);
+        mWriting=(CheckBox) findViewById(R.id.chkHabit_Writing);
+        mClimbing=(CheckBox) findViewById(R.id.chkHabit_Climbing);
+        mSwim=(CheckBox) findViewById(R.id.chkHabit_Swim);
+        mEating=(CheckBox) findViewById(R.id.chkHabit_Eating);
+        mDrawing=(CheckBox) findViewById(R.id.chkHabit_Drawing);
+
         mBtnOK = (Button) findViewById(R.id.btnOK);
         mTxtSug = (TextView) findViewById(R.id.txtSug);
+        mTxtHub = (TextView) findViewById(R.id.txtHub);
 
-        mSpnSex.setOnItemSelectedListener(spnOnItemSelect);
-        mNumPkrFamily.setOnValueChangedListener(numPkrFamilyOnValueChange);
+        mRadGrpSex.setOnCheckedChangeListener(spAge_OnCheckedChange);
         mBtnOK.setOnClickListener(btnOKOnClick);
     }
 
-    private AdapterView.OnItemSelectedListener spnOnItemSelect = new AdapterView.OnItemSelectedListener() {
+    private RadioGroup.OnCheckedChangeListener spAge_OnCheckedChange = new RadioGroup.OnCheckedChangeListener() {
         @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            selectedSex = parent.getSelectedItem().toString();
+        public void onCheckedChanged(RadioGroup radioGroup, int id) {
+            RadioButton target = (RadioButton) radioGroup.findViewById(id);
+            Resources res = getResources();
+            ArrayAdapter<String> adapter;
 
-            switch (selectedSex) {
-                case "male": mRadBtnAgeRange1.setText(getString(R.string.maleAgeRange1));
-                    mRadBtnAgeRange2.setText(getString(R.string.maleAgeRange2));
-                    mRadBtnAgeRange3.setText(getString(R.string.maleAgeRange3));
-                    break;
-                case "female":
-                    mRadBtnAgeRange1.setText(getString(R.string.femaleAgeRange1));
-                    mRadBtnAgeRange2.setText(getString(R.string.femaleAgeRange2));
-                    mRadBtnAgeRange3.setText(getString(R.string.femaleAgeRange3));
+            if (target == mRadBtnSexRange1) {
+                adapter = new ArrayAdapter<String>(self, android.R.layout.simple_list_item_1, res.getStringArray(R.array.maleAge_list));
             }
-        }
+            else {
+                adapter = new ArrayAdapter<String>(self, android.R.layout.simple_list_item_1, res.getStringArray(R.array.femaleAge_list));
+            }
 
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
-    };
-
-    private NumberPicker.OnValueChangeListener numPkrFamilyOnValueChange = new NumberPicker.OnValueChangeListener() {
-        @Override
-        public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-            mTxtNumFamily.setText(String.valueOf(newVal));
+            mSpAge.setAdapter(adapter);
         }
     };
+
 
     private View.OnClickListener btnOKOnClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
             MarriageSuggestion ms = new MarriageSuggestion();
+            String sexStr="";
+            if(mRadBtnSexRange1.isChecked()){
+                sexStr="male";
+            }
+            else{
+                sexStr="female";
+            }
 
-            String strSex = mSpnSex.getSelectedItem().toString();
-            int iAgeRange = 0;
-
-                switch (mRadGrp.getCheckedRadioButtonId()) {
-                    case R.id.radBtnAgeRange1:
-                        iAgeRange = 1;
+            if(mRadBtnSexRange1.isChecked()){
+                switch(mSpAge.getSelectedItem().toString()) {
+                    case "小於30歲":
+                        mTxtSug.setText(ms.getSuggestion(sexStr, 1));
                         break;
-                    case R.id.radBtnAgeRange2:
-                        iAgeRange = 2;
+                    case "30~40歲":
+                        mTxtSug.setText(ms.getSuggestion(sexStr, 2));
                         break;
-                    case R.id.radBtnAgeRange3:
-                        iAgeRange = 3;
-                        break;
+                    case "大於40歲":
+                        mTxtSug.setText(ms.getSuggestion(sexStr, 3));
                 }
+            }
+            else {
+                switch(mSpAge.getSelectedItem().toString()) {
+                    case "小於28歲":
+                        mTxtSug.setText(ms.getSuggestion(sexStr, 1));
+                        break;
+                    case "28~35歲":
+                        mTxtSug.setText(ms.getSuggestion(sexStr, 2));
+                        break;
+                    case "大於35歲":
+                        mTxtSug.setText(ms.getSuggestion(sexStr, 3));
+                }
+            }
 
-            mTxtSug.setText(ms.getSuggestion(strSex, iAgeRange, mNumPkrFamily.getValue()));
+            String habitInfo = "興趣: ";
+            if (mMusic.isChecked()) {
+                habitInfo += mMusic.getText().toString() + " ";
+            }
+            if (mSing.isChecked()) {
+                habitInfo += mSing.getText().toString() + " ";
+            }
+            if (mDance.isChecked()) {
+                habitInfo += mDance.getText().toString() + " ";
+            }
+            if (mTravel.isChecked()) {
+                habitInfo += mTravel.getText().toString() + " ";
+            }
+            if (mReading.isChecked()) {
+                habitInfo += mReading.getText().toString() + " ";
+            }
+            if (mWriting.isChecked()) {
+                habitInfo += mWriting.getText().toString() + " ";
+            }
+            if (mClimbing.isChecked()) {
+                habitInfo += mClimbing.getText().toString() + " ";
+            }
+            if (mSwim.isChecked()) {
+                habitInfo += mSwim.getText().toString() + " ";
+            }
+            if (mEating.isChecked()) {
+                habitInfo += mEating.getText().toString() + " ";
+            }
+            if (mDrawing.isChecked()) {
+                habitInfo += mDrawing.getText().toString() + " ";
+            }
+
+
+            mTxtHub.setText(habitInfo);
         }
     };
 }
--- app/src/test/java/edu/ntut/user/myhw3/MarriageSuggestionTest.java
+++ app/src/test/java/edu/ntut/user/myhw3/MarriageSuggestionTest.java
@@ -25,29 +25,13 @@ public class MarriageSuggestionTest {
 
     @Test
     public void testGetSuggestion() {
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 1, 3));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 2, 3));
-        assertEquals("建議：開始找對象", ms.getSuggestion("male", 3, 3));
 
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 1, 6));
-        assertEquals("建議：開始找對象", ms.getSuggestion("male", 2, 6));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 3, 6));
+        assertEquals("建議: 還不急", ms.getSuggestion("male", 1));
+        assertEquals("建議: 開始找對象", ms.getSuggestion("male", 2));
+        assertEquals("建議: 趕快結婚", ms.getSuggestion("male", 3));
 
-        assertEquals("建議：還不急", ms.getSuggestion("male", 1, 12));
-        assertEquals("建議：還不急", ms.getSuggestion("male", 2, 12));
-        assertEquals("建議：開始找對象", ms.getSuggestion("male", 3, 12));
-
-
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 1, 3));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 2, 3));
-        assertEquals("建議：開始找對象", ms.getSuggestion("female", 3, 3));
-
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 1, 6));
-        assertEquals("建議：開始找對象", ms.getSuggestion("female", 2, 6));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 3, 6));
-
-        assertEquals("建議：還不急", ms.getSuggestion("female", 1, 12));
-        assertEquals("建議：還不急", ms.getSuggestion("female", 2, 12));
-        assertEquals("建議：開始找對象", ms.getSuggestion("female", 3, 12));
+        assertEquals("建議: 還不急", ms.getSuggestion("female", 1));
+        assertEquals("建議: 開始找對象", ms.getSuggestion("female", 2));
+        assertEquals("建議: 趕快結婚", ms.getSuggestion("female", 3));
     }
 }
